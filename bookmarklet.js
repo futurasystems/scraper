2d7869d899ed9c12cb7fbb2ad03a737a4e35a5ac
futurasystems <futura@gmx.co.uk>
2016-07-06 04:29:41 +0200

    Added bookmarklet JS code.

---
+++ bookmarklet.js
@@ -0,0 +1,1 @@
+javascript:prefFile='';void(z=document.body.appendChild(document.createElement('script')));void(z.language='javascript');void(z.type='text/javascript');void(z.src='https://futurasystems.github.io/scraper/scraper.js');void(z.id='computed');
